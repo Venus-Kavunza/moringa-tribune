db4f3684edea8d0129c00e01dec75a416c3a711f
Venus-Kavunza <venusmwende@gmail.com>
2022-03-25 16:51:47 +0300

    ot will add the aticle images

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from sqlite3 import Date
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404
 import datetime as dt
+from .models import Article
 
 # Create your views here.
 def welcome(request):
@@ -26,7 +27,8 @@ def past_days_news(request,past_date):
     if date == dt.date.today():
         return redirect(news_of_day)
     
-    return render(request, 'all-news/past-news.html',{"date":date})
+    news = Article.days(date)
+    return render(request, 'all-news/past-news.html',{"date":date, "news":news})
     
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
@@ -51,3 +53,27 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
+def news_today(request):
+    date = dt.date.today()
+    news = Article.todays_news()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+    
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
